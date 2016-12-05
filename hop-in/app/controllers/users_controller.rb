4bfde168e05c9896d833489549c1dbbc13164845
terious78 <taesuk.moon@gmail.com>
2016-12-05 15:47:48 -0500

    Created methods in user controller

--- hop-in/app/controllers/users_controller.rb
+++ hop-in/app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController
-  
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def user_params()
+
+  end
 end
