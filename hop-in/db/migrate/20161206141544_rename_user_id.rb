689d4dfc8c3472a48626f93bd680a786a6e32858
Tyler <tcameronsmith@gmail.com>
2016-12-06 12:13:00 -0500

    change passenger_id to user_id in reservations table, and owner_id to user_id in vehicle table

---
+++ hop-in/db/migrate/20161206141544_rename_user_id.rb
@@ -0,0 +1,14 @@
+class RenameUserId < ActiveRecord::Migration[5.0]
+	def change
+		reversible do |dir|
+			dir.up do
+				rename_column :reservations, :passenger_id, :user_id
+				rename_column :vehicles, :owner_id, :user_id
+			end
+			dir.down do
+				rename_column :reservations, :user_id, :passenger_id
+				rename_column :vehicles, :user_id, :owner_id
+			end
+		end
+	end
+end
