017bd442a81d4229a21e97bddb8c34cf1a9b6373
Tyler <tcameronsmith@gmail.com>
2016-12-06 08:57:47 -0500

    add session params to session controller

--- hop-in/app/controllers/sessions_controller.rb
+++ hop-in/app/controllers/sessions_controller.rb
@@ -1,21 +1,25 @@
 class SessionsController < ApplicationController
 
-  def new
+	def new
+	end
 
-  end
+	def create
+		user = User.find_by(email: session_params[:email])
+		if user && user.authenticate(session_params[:password])
+			session[:user_id] = user.id
+			redirect_to vehicles_url, alert: "You are now logged In :)"
+		else
+			render :new
+		end
+	end
 
-  def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user
-      redirect_to vehicles_url, alert: "You are now logged In :)"
-    else
-      render :new
-    end
-  end
+	def destroy
+		session[:user_id] = nil
+		redirect_to users_url, alert: "You are logged out!"
+	end
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to users_url, alert: "You are logged out!"
-  end
+	private
+	def session_params
+		params.require(:session).permit(:email, :password)
+	end
 end
