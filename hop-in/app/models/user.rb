358054ac08a83829b009fb44cdcc67fb0f308f7d
Tyler <tcameronsmith@gmail.com>
2016-12-06 13:29:00 -0500

    change seeds, add comment to user model

--- hop-in/app/models/user.rb
+++ hop-in/app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
 	has_secure_password
-
+	#owner
 	has_many :owned_vehicles, class_name: 'Vehicle', source: :vehicle
 	has_many :vehicle_reservations, class_name: 'Reservation', source: :reservation, through: :owned_vehicles
 	has_many :passengers, class_name: 'User', source: :user, through: :owned_vehicles
-
+	#passenger
 	has_many :trips, class_name: 'Reservation', source: :reservation
 	has_many :rented_vehicles, class_name: 'Vehicle', source: :vehicle, through: :trips
 	has_many :owners, class_name: 'User', source: :user, through: :trips
