2e2bafeb2df264d89c5d0c958ce8e522a7521279
Tyler <tcameronsmith@gmail.com>
2016-12-06 17:25:32 -0500

    create migration to add seats to vehicle table

--- hop-in/db/schema.rb
+++ hop-in/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206141544) do
+ActiveRecord::Schema.define(version: 20161206221953) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "passenger_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20161206141544) do
     t.integer  "owner_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "seats"
   end
 
 end
