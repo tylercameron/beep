e0c07245558c3a60729c1211255850d0545fca8a
Tyler <tcameronsmith@gmail.com>
2016-12-06 17:26:22 -0500

    add seats to vehicle validations

--- hop-in/app/models/vehicle.rb
+++ hop-in/app/models/vehicle.rb
@@ -3,5 +3,5 @@ class Vehicle < ApplicationRecord
 	has_many :passengers, class_name: 'User', through: :reservations
 	belongs_to :owner, class_name: 'User'
 
-	validates_presence_of :vehicle_year, :vehicle_make, :vehicle_model, :street_address, :city, :province, :postal_code
+	validates_presence_of :vehicle_year, :vehicle_make, :vehicle_model, :street_address, :city, :province, :postal_code, :seats
 end
