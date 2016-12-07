8bd158056747762ea160709e743b7018a1ef07e1
Tyler <tcameronsmith@gmail.com>
2016-12-06 19:50:05 -0500

    create available method in vehicle model, checks number of passengers is less than seats, called in reservation create

--- hop-in/app/models/vehicle.rb
+++ hop-in/app/models/vehicle.rb
@@ -4,4 +4,13 @@ class Vehicle < ApplicationRecord
 	belongs_to :owner, class_name: 'User'
 
 	validates_presence_of :vehicle_year, :vehicle_make, :vehicle_model, :street_address, :city, :province, :postal_code, :seats
+
+	def available(number_of_passengers, vehicle_id)
+		number_of_passengers > 0 && seat_capacity(vehicle_id) >= number_of_passengers
+	end
+
+private
+	def seat_capacity(vehicle_id)
+		Vehicle.where(id: vehicle_id).first.capacity
+	end
 end
