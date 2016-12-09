9fe38e56124c416db4131f67b13fb210f3c699c5
Tyler <tcameronsmith@gmail.com>
2016-12-08 20:17:12 -0500

    add geocoder function to vehicle index view, populates map with marker, added latitude and longitude to vehicle table with migration

--- hop-in/db/schema.rb
+++ hop-in/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206231844) do
+ActiveRecord::Schema.define(version: 20161209000827) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "passenger_id"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20161206231844) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "seats"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
