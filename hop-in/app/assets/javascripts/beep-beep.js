7c9f950cbd40cad35f2f670445dd47873aafcd0d
Tyler <tcameronsmith@gmail.com>
2016-12-14 18:27:24 -0500

    reformat line spacing on js page

--- hop-in/app/assets/javascripts/beep-beep.js
+++ hop-in/app/assets/javascripts/beep-beep.js
@@ -1,31 +1,24 @@
 $(document).ready(function() {
 
-$('.login-btn').on('click', function() {
+  $('.login-btn').on('click', function() {
 
-  $('.login-btn').fadeOut('fast');
-  $('.signup-btn').fadeOut('fast');
-  $('.login').fadeIn('slow');
-});
+    $('.login-btn').fadeOut('fast');
+    $('.signup-btn').fadeOut('fast');
+    $('.login').fadeIn('slow');
+  });
 
-$('.signup-btn').on('click', function() {
+  $('.signup-btn').on('click', function() {
 
-  $('.login-btn').fadeOut('fast');
-  $('.signup-btn').fadeOut('fast');
-  $('.signup-container').fadeIn('slow');
-});
+    $('.login-btn').fadeOut('fast');
+    $('.signup-btn').fadeOut('fast');
+    $('.signup-container').fadeIn('slow');
+  });
 
-$('.map-btn').on('click', function() {
-  $('.map-div').fadeIn('slow');
-  $('#map').fadeIn('slow');
-  $('.map-btn').fadeOut('fast');
-  initMap();
-});
-
-
-
-
-
-
-
+  $('.map-btn').on('click', function() {
+    $('.map-div').fadeIn('slow');
+    $('#map').fadeIn('slow');
+    $('.map-btn').fadeOut('fast');
+    initMap();
+  });
 
 });
