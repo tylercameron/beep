1156f153b32c643d5ded82577551a8d603b0f826
Jessica <jhai_alex@hotmail.com>
2016-12-14 16:12:03 -0500

    added fadein function to signup form, added fadeout function to link

--- hop-in/app/assets/javascripts/beep-beep.js
+++ hop-in/app/assets/javascripts/beep-beep.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
 
 $('.login-btn').on('click', function() {
-  $('.login').fadeIn('slow');
+
   $('.login-btn').fadeOut('fast');
   $('.signup-btn').fadeOut('fast');
+  $('.login').fadeIn('slow');
+});
+
+$('.signup-btn').on('click', function() {
+
+  $('.login-btn').fadeOut('fast');
+  $('.signup-btn').fadeOut('fast');
+  $('.signup-container').fadeIn('slow');
 });
 
 
