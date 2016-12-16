6540d35ec3996da85c7f9663d355149bd818895b
Jessica <jhai_alex@hotmail.com>
2016-12-16 14:28:10 -0500

    added fadeout js function to the close button on both login and signup form

--- hop-in/app/assets/javascripts/beep-beep.js
+++ hop-in/app/assets/javascripts/beep-beep.js
@@ -16,6 +16,16 @@ $(document).ready(function() {
     $('.signup-container').fadeIn('slow');
   });
 
+  $('.close').on('click', function() {
+
+    $('.login').fadeOut('fast');
+    $('.signup-container').fadeOut('fast');
+    $('.login-btn').fadeIn('slow');
+    $('.signup-btn').fadeIn('slow');
+    $('.homepage-search').fadeIn('slow');
+
+  });
+
   $('.map-btn').on('click', function() {
     $('.map-div').fadeIn('slow');
     $('#map').fadeIn('slow');
