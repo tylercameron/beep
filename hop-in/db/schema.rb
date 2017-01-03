ffd57a3c7b62f8e006e8ebeea3a16568966e4f18
Tyler <tcameronsmith@gmail.com>
2017-01-03 14:45:04 -0500

    add migration to add table for availabilities

--- hop-in/db/schema.rb
+++ hop-in/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216162501) do
+ActiveRecord::Schema.define(version: 20170103191907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "availabilities", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.integer  "vehicle_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "passenger_id"
