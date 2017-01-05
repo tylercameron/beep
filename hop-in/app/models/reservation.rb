6a409fce1cb2c8efa7f8898de6e99357893cc9e0
Tyler <tcameronsmith@gmail.com>
2017-01-05 16:04:22 -0500

    create available method

--- hop-in/app/models/reservation.rb
+++ hop-in/app/models/reservation.rb
@@ -4,7 +4,7 @@ class Reservation < ApplicationRecord
 	belongs_to :passenger, class_name: 'User'
 
 	validates_presence_of :start_time, :end_time, :start_location, :destination, :seats
-  validate :not_past_date, :capacity, :available
+  validate :not_past_date, :capacity, :not_booked, :available
 
   def not_past_date
     if self.start_time < DateTime.now
@@ -21,7 +21,7 @@ class Reservation < ApplicationRecord
     end
   end
 
-  def available
+  def not_booked
     self.vehicle.reservations.each do |booked|
       if booked.start_time <= self.end_time && self.start_time <= booked.end_time
         errors.add(:start_time, "Vehicle not available at this time")
@@ -32,4 +32,15 @@ class Reservation < ApplicationRecord
     end
   end
 
+  def available
+    self.vehicle.availabilities.each do |availability|
+      if self.start_time.strftime("%A") == availability.weekday && self.end_time.strftime("%A") == availability.weekday
+        if self.start_time.strftime("%k%M").to_i >= availability.start_time || self.end_time.strftime("%k%M").to_i <= availability.end_time
+          return true
+        end
+      else
+        return false
+      end
+    end
+  end
 end
