6c84633fd3fc5a908912af3479614d1cda2101af
Jessica <jhai_alex@hotmail.com>
2017-01-11 14:29:37 -0500

    added redirect back method to sessions controller for login and sign up and reservation page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: session_params[:email])
 		if user && user.authenticate(session_params[:password])
 			session[:user_id] = user.id
-			redirect_to vehicles_url, notice: "You are now logged In :)"
+			redirect_back fallback_location: vehicles_url, notice: "You are now logged In :)"
 		else
 			flash[:notice] = "Invalid Email or Password"
 			redirect_to new_user_url
