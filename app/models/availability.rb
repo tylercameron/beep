99c278130985f864ca23623e1e57562be56f46de
Tyler <tcameronsmith@gmail.com>
2017-01-13 18:56:17 -0500

    add type of validation

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -2,5 +2,5 @@ class Availability < ApplicationRecord
   has_one :owner, class_name: 'User', through: :vehicle
   belongs_to :vehicle
 
-  validates :start_time, :end_time
+  validates :start_time, :end_time, presence: true
 end
